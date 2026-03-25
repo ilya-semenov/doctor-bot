a6f47dee5e2d7a4496fb820e6410e223d73636dc
ilya-semenov <ilywa-777@mail.ru>
2026-03-25 19:41:39 +0500

    Delete states.py

--- states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class DoctorConsultation(StatesGroup):
-    age = State()           # состояние ожидания возраста
-    gender = State()        # состояние ожидания пола
-    conversation = State()  # состояние непрерывного диалога
